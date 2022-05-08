cff45a4de2120549ba5e733b56686036b1fb731e
vijayk1431 <kounsalyevijay1234@gmail.com>
2022-05-08 14:04:55 +0530

    vkvkvk

---
+++ Annotation2 - Copy.java
@@ -0,0 +1,21 @@
+package TestNg;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Annotation2 
+{
+	//dependsOnMethod
+	    @Test(dependsOnMethods={"cbb"})//if cbb() method fail b() method also fail. 
+	    public void b()
+	    {
+	    	System.out.println("after suite");
+	    }
+	//Always Run123234
+	    @Test(invocationCount=5)//if u want to run multiple times then invocation count=5
+	    public void cbb()
+	    {
+	    	System.out.println("test1");
+	    	Assert.fail();//assert is a class fail() is a method to abort the execution 
+	    }
+	
+
+}
